fb267273485ed9f987b3c904bc2e204bce989dab
francis <15011053797@163.com>
2019-01-17 18:21:04 +0800

    version 1.17
    this is simple chatroom for 1.1.17.

--- qqClient.py
+++ qqClient.py
@@ -8,15 +8,55 @@ def main():
         print('参数错误！')
         return
 
-    address=(sys.argv[1],int(sys.argv[2]))
+    address = (sys.argv[1],int(sys.argv[2]))
     #创建 UDP 套接字
-    client=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-    #发送消息
-    client.sendto('francis'.encode(),address)
-    #接收消息
-    data,addr=client.recvfrom(1024)
-    print(data.decode())
+    client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
+    #接收用户输入，包装后发送给服务器
+    while True:
+        name = input('请输入姓名：')
+        message = 'login ' + name#此处加入标记
+        client.sendto(message.encode(),address)
+        data,addr = client.recvfrom(1024)
+        if data.decode() == 'OK':
+            print('您已经进入聊天室...')
+            break
+
+        else:#不允许进入
+            #打印不允许进入的原因
+            print(data.decode())
+
+        #创建进程
+    pid = os.fork()
+    if pid<0:
+        sys.exit('创建进程失败！')
+
+    elif pid == 0:
+        sendmsg(client,name,address)
+
+    else:
+        recvmsg(client)
+
+def sendmsg(client,name,address):
+    #发送消息给服务器，服务器群发给所有客户端
+    while True:
+        content=input('请发言（输入quit 退出）：')
+        if content == 'quit':
+            message =  'quit ' + name
+            client.sendto(message.encode(),address)
+            sys.exit('已退出聊天室')#子进程退出
+
+        #包装消息
+        message = 'speak %s %s' % (name,content)
+        client.sendto(message.encode(),address)
+    
+
+def recvmsg(client):
+    while True:
+        message,addr = client.recvfrom(1024)
+        if message.decode() == 'exit':#如果收到服务器此消息，父进程退出
+            os._exit(0)
+        print(message.decode())
 
 if __name__ == "__main__":
     main()
--- qqServer.py
+++ qqServer.py
@@ -12,11 +12,11 @@ import sys
 #创建网络连接
 def main():
     #创建 UDP 套接字
-    server=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#设置端口复用
+    server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)#设置端口复用
 
     server.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)#绑定地址
     
-    address=(('0.0.0.0',8888))
+    address = (('0.0.0.0',8888))
     server.bind(address)
     #创建多线程，让子进程负责管理员喊话，父进程和客户端交互
     
@@ -26,8 +26,8 @@ def main():
         print('进程创建失败！')
         return
 
-    elif pid==0:
-        print('我是负责管理员喊话的进程')
+    elif pid == 0:#子进程的任务
+        print('我是子进程，我负责管理员喊话')
 
     else:#父进程处理客户端的各种请求
         doRequest(server)
@@ -35,14 +35,58 @@ def main():
 
 #处理客户端请求的函数
 def doRequest(server):
+    userlist={}#此字典用于存放客户端的名字和地址
+    while True:
     #接收来自客户端的消息
-    data,addr=server.recvfrom(1024)
-    print(data.decode())
+        msg,addr = server.recvfrom(1024)
+        msglist = msg.decode().split(' ')#拆分数据,以空格为分隔
+        if msglist[0] == 'login':#如果是进入聊天室请求
+            doLogin(server,userlist,msglist[1],addr)
+        elif msglist[0] == 'speak':
+            #msglist:['c','name','content']
+            content = ' '.join(msglist[2:])#获取完整发送内容
+            #发送给其他所有成员
+            doChat(server,content,userlist,msglist[1])
 
-    #向客户端发送消息
-    server.sendto('服务器已收到'.encode(),addr)
+        elif msglist[0] == 'quit':
+            doQuit(server,msglist[1],userlist)
+
+#客户端退出处理函数
+def doQuit(server,name,userlist):
+    message = '%s 退出了聊天室' %name
+    for u in userlist:
+        if u!=name:
+            server.sendto(message.encode(),userlist[u])
+        else:
+            server.sendto('exit'.encode(),userlist[name])
+        #从存储结构中删除
+    del userlist[name]
+
+        
+#用于聊天的函数（把内容发送给其他成员）
+def doChat(server,content,userlist,name):
+    message = '%s 说：%s' % (name,content)
+    for u in userlist:
+        if u!= name:#发给不是自身的所有客户端
+            server.sendto(message.encode(),userlist[u])
 
 
+#列表用字典存储{name,addr}
+#进入聊天室请求处理函数
+def doLogin(server,userlist,name,addr):
+    #判断姓名是否同名
+    if (name in userlist) or name == '管理员':#如果列表中已经存在此用户名,或取名为管理员
+        server.sendto('该用户已经存在！'.encode(),addr)
+        return#函数结束，返回继续接收新的数据
+
+    #同名不存在，发送信号给客户端，运行进入
+    server.sendto('OK'.encode(),addr)
+    #通知所有人
+    message='欢迎%s进入聊天室'%name
+    for u in userlist:
+        server.sendto(message.encode(),userlist[u])#全发
+    userlist[name]=addr#加入到存储结构userlist字典中
+    print(userlist)
 
 
 if __name__ == '__main__':
